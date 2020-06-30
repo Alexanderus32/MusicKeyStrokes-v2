221c11f0c2bc28c4f7f08835967919185eee45d7
HagBcemu <vlad051198vlad@gmail.com>
2020-06-30 18:42:56 +0300

    Change KeyModel

--- MusicKeyStrokes/Models/Key.cs
+++ MusicKeyStrokes/Models/Key.cs
@@ -4,16 +4,22 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.IO;
+using System.Windows.Forms;
 
 namespace MusicKeyStrokes.Models
 {
 
-    class Key
+    class KeyModel
     {
         public LayoutSound Layout { get; set; }
-        public string KeyValue { get; set; }
+
+        public Keys KeyValue { get; set; }
+
         public string NameSound { get; set; }
+
         public string PathSound { get; set; }
+
+        public string VoiceCommand { get; set; }
 
     }
 }
