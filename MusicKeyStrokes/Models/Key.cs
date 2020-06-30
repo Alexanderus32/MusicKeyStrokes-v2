efefd1b5524e12804e74b70dda0ee6b3ceba058d
HagBcemu <vlad051198vlad@gmail.com>
2020-06-30 18:26:09 +0300

    added model for working

---
+++ MusicKeyStrokes/Models/Key.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace MusicKeyStrokes.Models
+{
+
+    class Key
+    {
+        public LayoutSound Layout { get; set; }
+        public string KeyValue { get; set; }
+        public string NameSound { get; set; }
+        public string PathSound { get; set; }
+
+    }
+}
