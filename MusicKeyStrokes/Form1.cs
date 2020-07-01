cc6d8eccd03db44d72c28afd295dfd2790bf2003
SaberNero <aleksandr.pyshnyi@nixsolutions.com>
2020-07-01 15:55:35 +0300

    Changed part 2

--- MusicKeyStrokes/Form1.cs
+++ MusicKeyStrokes/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MusicKeyStrokes.Models;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +16,21 @@ namespace MusicKeyStrokes
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //using serializer 
+            JsonSerializer sr = new JsonSerializer();
+            var music = sr.Deserialize<KeyModel>();
+
+            if (music == null)
+                music = new List<KeyModel>();
+
+            var model = new KeyModel() { NameSound = "ban", KeyValue = Keys.Q, Layout = LayoutSound.Valakas1,PathSound = "music/valakas1/ban.mp3" };
+            music.Add(model);
+            sr.Serialize<KeyModel>(music);
+            
         }
     }
 }
