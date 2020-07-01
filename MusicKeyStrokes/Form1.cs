266fc3bf7e343ca18bf7d901afb95fdfa333393d
HagBcemu <vlad051198vlad@gmail.com>
2020-07-01 19:23:45 +0300

    I don't understand what i am doing

--- MusicKeyStrokes/Form1.cs
+++ MusicKeyStrokes/Form1.cs
@@ -12,9 +12,23 @@ namespace MusicKeyStrokes
 {
     public partial class Form1 : Form
     {
+
+        
         public Form1()
         {
             InitializeComponent();
+            WatcherHotKeys.WatcherArrayHotKey();
+        }
+
+
+        protected override void WndProc(ref Message m)
+        {
+           if (m.Msg == 0x0312)
+           {
+              WatcherHotKeys watcher = new WatcherHotKeys();
+              watcher.Keyhandler(m);
+           }
+            base.WndProc(ref m);
         }
     }
 }
